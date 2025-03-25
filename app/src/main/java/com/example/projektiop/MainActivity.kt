e402f777fcc0ed8b7d5e01a1cb653a0dba7a97f9
Venphis <165069478+Venphis@users.noreply.github.com>
2025-03-25 14:24:05 +0100

    menu

--- app/src/main/java/com/example/projektiop/MainActivity.kt
+++ app/src/main/java/com/example/projektiop/MainActivity.kt
@@ -1,16 +1,26 @@
 package com.example.projektiop
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.projektiop.ui.theme.ProjektIOPTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +31,7 @@ class MainActivity : ComponentActivity() {
             ProjektIOPTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = (0..999999).random().toString(),
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -30,18 +40,83 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+//Główny ekran
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column(modifier = modifier.fillMaxSize(),
+        horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(16.dp)){
+        Text(text = "Your name is $name!", modifier = modifier)
+        StartAdvertisingButton()
+        StartScanningButton()
+        StopAdvertisingButton()
+        StopScanningButton()
+        Status()
+    }
+
 }
+
+//Tutaj przycisk do startu nadawania
+@Composable
+fun StartAdvertisingButton(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    Button(onClick = {
+        Toast.makeText(context, "Rozpoczęto nadawanie", Toast.LENGTH_SHORT).show()
+    }, modifier = modifier) {
+        Text(text = "Rozpocznij nadawanie")
+    }
+}
+
+//Tutaj przycisk do startu skanowania
+@Composable
+fun StartScanningButton(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    Button(onClick = {
+        Toast.makeText(context, "Rozpoczęto skanowanie", Toast.LENGTH_SHORT).show()
+    }, modifier = modifier) {
+        Text(text = "Rozpocznij skanowanie")
+    }
+}
+
+//Tutaj przycisk do zatrzymania nadawania
+@Composable
+fun StopAdvertisingButton(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    Button(onClick = {
+        Toast.makeText(context, "Zatrzymano nadawanie", Toast.LENGTH_SHORT).show()
+    }, modifier = modifier) {
+        Text(text = "Zatrzymaj nadawanie")
+    }
+}
+
+//Tutaj przycisk do zatrzymania skanowania
+@Composable
+fun StopScanningButton(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    Button(onClick = {
+        Toast.makeText(context, "Zatrzymano nadawanie", Toast.LENGTH_SHORT).show()
+    }, modifier = modifier) {
+        Text(text = "Zatrzymaj skanowanie")
+    }
+}
+
+//Tutaj pokazuj status czyli np polaczono z itp
+@Composable
+fun Status(text: String = "brak", modifier: Modifier = Modifier) {
+    var importText by remember{
+        mutableStateOf(text)
+    }
+
+    var statusText = "Status: "+ importText
+
+    Text(text = statusText, modifier = modifier)
+}
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DefaultPreview() {
     ProjektIOPTheme {
-        Greeting("Android")
+        Greeting(12345.toString())
     }
 }
