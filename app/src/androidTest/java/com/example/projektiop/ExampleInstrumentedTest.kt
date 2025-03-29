9df141eb7f831fc2a5547900b8c1de154f6b10a8
bart-soj <bartoszsojka2@gmail.com>
2025-03-29 14:12:31 +0100

    rebase onto nya after eng locale added

--- app/src/androidTest/java/com/example/projektiop/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/projektiop/ExampleInstrumentedTest.kt
@@ -2,6 +2,7 @@ package com.example.projektiop
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.projektiop.activeHandshake.NFC.HostBasedCardEmulatorActivity
 
 import org.junit.Test
 import org.junit.runner.RunWith
