a9b93e35c5f0fd3e15e9c4bba7a12e8992d62322
Hakaitte <h4kaittemail@gmail.com>
2025-06-12 08:01:10 +0000

    update

--- app/src/main/java/com/example/projektiop/ui/theme/Type.kt
+++ app/src/main/java/com/example/projektiop/ui/theme/Type.kt
@@ -1,6 +1,6 @@
 package com.example.projektiop.ui.theme
 
-import androidx.compose.material3.Typography
+import androidx.compose.material3.Typography 
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
