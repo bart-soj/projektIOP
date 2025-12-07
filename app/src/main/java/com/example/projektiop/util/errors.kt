42cf51392128c646bbfb815dd51e3a797e561e31
bart-soj <bartoszsojka2@gmail.com>
2025-12-08 00:16:43 +0100

    add custom Result and Errors to use with it

---
+++ app/src/main/java/com/example/projektiop/util/errors.kt
@@ -0,0 +1,41 @@
+package com.example.projektiop.util
+
+import io.realm.kotlin.internal.util.Validation
+
+sealed interface RootError
+
+sealed interface DataError: RootError {
+    enum class Network: DataError {
+        REQUEST_TIMEOUT,
+        TOO_MANY_REQUESTS,
+        NO_INTERNET,
+        PAYLOAD_TOO_LARGE,
+        SERVER_ERROR,
+        SERIALIZATION,
+        UNKNOWN
+    }
+    enum class Local: DataError {
+        DISK_FULL,
+        DB_ERROR
+    }
+}
+
+sealed interface ValidationError: RootError {
+    enum class Common: ValidationError {
+        BLANK
+    }
+
+    enum class PasswordError: ValidationError {
+        TOO_SHORT,
+        NO_UPPERCASE,
+        NO_DIGIT,
+        NO_LOWERCASE,
+    }
+    enum class EmailError: ValidationError {
+        NOT_EMAIL,
+    }
+    enum class UsernameError: ValidationError {
+    }
+}
+
+
